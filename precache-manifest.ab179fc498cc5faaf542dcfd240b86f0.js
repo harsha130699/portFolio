287670117f110259e4aef814142eb3a1dbd9a547
Balanagu Harsha Vardhan <t-bavard@microsoft.com>
2020-05-21 21:41:38 +0530

    Updates

---
+++ precache-manifest.ab179fc498cc5faaf542dcfd240b86f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca370b235980c39851590d80f3c4b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4072ac87bbb62a933949",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "089ff320a5ef20d178a1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4072ac87bbb62a933949",
+    "url": "/static/js/2.6a47dcef.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.6a47dcef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089ff320a5ef20d178a1",
+    "url": "/static/js/main.34689fed.chunk.js"
+  },
+  {
+    "revision": "bbb5d70d7dc6347c4839",
+    "url": "/static/js/runtime-main.834ba605.js"
+  }
+]);
